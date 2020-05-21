da8448884e0e2ebd30b64554b5ac6f4b108f5328
Aditya Shah <adi.shah0912@gmail.com>
2020-05-21 11:59:45 +0530

    Check for error and receive data from server

--- tcp_client.c
+++ tcp_client.c
@@ -16,6 +16,18 @@ int main() {
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(9002);
     server_address.sin_addr.s_addr = INADDR_ANY;
-      
+    
+    //Connection
+    int connnection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+    
+    //Check for error
+    if(connnection_status == -1 )
+        printf("there is an error making a connection to the remote socket");
+
+    // Recieve data from the server
+    char server_response[256];
+    recv(network_socket, &server_response, sizeof(server_response), 0);
+
+    
     return 0;
 }
