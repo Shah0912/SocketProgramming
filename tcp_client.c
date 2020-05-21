26c01d0d4227164bdefb3e66de759e8f37f4fc2c
Aditya Shah <adi.shah0912@gmail.com>
2020-05-21 11:27:30 +0530

    Initial commit

---
+++ tcp_client.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include<sys/types.h>
+#include<sys/socket.h>
+
+#include<netinet/in.h>
+
+int main() {
+    //Create a socket
+    int network_socket;
+    network_socket = socket(AF_INET, )
+    return 0;
+}
