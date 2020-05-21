33b84de634a2f4aac40c88554f606efc51f66265
Aditya Shah <adi.shah0912@gmail.com>
2020-05-21 11:36:09 +0530

    Added address struct

--- tcp_client.c
+++ tcp_client.c
@@ -11,5 +11,11 @@ int main() {
     int network_socket;
     network_socket = socket(AF_INET, SOCK_STREAM, 0);
     
+    // Specify an address for the socket
+    struct sockaddr_in server_address; 
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(9002);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+      
     return 0;
 }
